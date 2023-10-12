42e3c4acf01478de36f8b82f70a2e1ec0f74eaca
asergeev <asergeev@rvision.ru>
2023-10-12 06:05:51 +0300

    а

--- src/main/java/org/example/KafkaTest.java
+++ src/main/java/org/example/KafkaTest.java
@@ -6,20 +6,18 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.listener.MessageListener;
-import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.kafka.test.context.EmbeddedKafkaCondition;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 
+import java.time.Duration;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -28,7 +26,6 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@ExtendWith(EmbeddedKafkaCondition.class)
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 public class KafkaTest {
 
@@ -37,17 +34,20 @@ public class KafkaTest {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
     private Consumer<String, String> consumer;
 
     private BlockingQueue<ConsumerRecord<String, String>> records;
 
     @BeforeEach
     public void setUp() {
-        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("testGroup", "false", embeddedKafka.getBrokersAsString());
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("testGroup", "false", embeddedKafkaBroker.getBrokersAsString());
         consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         DefaultKafkaConsumerFactory<String, String> cf = new DefaultKafkaConsumerFactory<>(consumerProps);
         consumer = cf.createConsumer();
-        embeddedKafka.consumeFromAnEmbeddedTopic(consumer, TEST_TOPIC);
+        embeddedKafkaBroker.consumeFromAnEmbeddedTopic(consumer, TEST_TOPIC);
         records = new LinkedBlockingQueue<>();
         consumer.subscribe(Collections.singletonList(TEST_TOPIC));
     }
@@ -71,6 +71,7 @@ public class KafkaTest {
         assertEquals(message, record.value());
     }
 }
+
 
 
 
